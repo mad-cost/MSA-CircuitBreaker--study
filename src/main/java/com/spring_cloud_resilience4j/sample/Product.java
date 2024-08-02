1b7373c3843749d32ca4f578304735f7d3205758
mad-cost <mad_scientist1998@naver.com>
2024-08-02 17:03:48 +0900

    CircuitBreaker와 Resilience4j

---
+++ src/main/java/com/spring_cloud_resilience4j/sample/Product.java
@@ -0,0 +1,16 @@
+package com.spring_cloud_resilience4j.sample;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Data // Getter and Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+  private String id; // 3
+  private String title; // 감사합니다
+
+}
